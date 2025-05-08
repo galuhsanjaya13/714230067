fb05973005032c75581dd4ced62e12470d8f7c45
Galuh <149026617+Galuhsanjayaputra@users.noreply.github.com>
2025-05-08 22:06:47 +0700

    update

--- belajar_golang/main.go
+++ belajar_golang/main.go
@@ -7,7 +7,7 @@ func main(){
 	app:=fiber.New()
 
 	app.Get("/",func (c *fiber.Ctx)error{
-		return c.SendString("hello , world! ")
+		return c.SendString("Hello , World! ")
 	})
 	log.Fatal(app.Listen(":3000"))
 }
